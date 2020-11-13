919729ca64c886a80570403f54cae871bb64aaa0
Djavid Gabibiyan <djavidoff@gmail.com>
2020-11-13 17:23:06 +0500

    fix: linux build scripts were missing  flag (#5)

--- scripts/build/build_gnu.sh
+++ scripts/build/build_gnu.sh
@@ -4,6 +4,6 @@ if [[ "$(docker images -q dashpay/neon-build:gnu 2> /dev/null)" == "" ]]; then
   docker build -f Dockerfile.gnu . -t dashpay/neon-build:gnu
 fi
 
-docker run -v $(pwd):/app dashpay/neon-build:gnu neon build
+docker run -v $(pwd):/app dashpay/neon-build:gnu neon build --release
 
 mv ./native/index.node ./prebuilds/linux-x64/node.abi72.glibc.node
