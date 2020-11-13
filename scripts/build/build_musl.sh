1a3b59ce70f225cbfb835bedb4cb99bc380362a3
Djavid Gabibiyan <djavidoff@gmail.com>
2020-11-13 17:19:14 +0500

    fix: linux build scripts were missing  flag

--- scripts/build/build_musl.sh
+++ scripts/build/build_musl.sh
@@ -4,6 +4,6 @@ if [[ "$(docker images -q dashpay/neon-build:musl 2> /dev/null)" == "" ]]; then
   docker build -f Dockerfile.musl . -t dashpay/neon-build:musl
 fi
 
-docker run -v $(pwd):/app dashpay/neon-build:musl neon build
+docker run -v $(pwd):/app dashpay/neon-build:musl neon build --release
 
 mv ./native/index.node ./prebuilds/linux-x64/node.abi72.musl.node
