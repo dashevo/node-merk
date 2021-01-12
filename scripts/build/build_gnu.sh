53dcc5bcfac98537a43c917f186c5fb456781942
Djavid Gabibiyan <djavidoff@gmail.com>
2021-01-12 12:47:46 +0500

    feat: update build scripts to be more re-usable

--- scripts/build/build_gnu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ "$(docker images -q dashpay/neon-build:gnu 2> /dev/null)" == "" ]]; then
-  docker build -f Dockerfile.gnu . -t dashpay/neon-build:gnu
-fi
-
-docker run -v $(pwd):/app dashpay/neon-build:gnu neon build --release
-
-mv ./native/index.node ./prebuilds/linux-x64/node.abi72.glibc.node
